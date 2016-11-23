966571c232727350a93938846e4ba5254b32ac1e
Jason <jdesigns.jason@gmail.com>
2016-11-23 06:06:47 -0600

    added routes, sass folder strucutre to begin breakdown

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,28 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }   from './app.component';
+import { RouterModule } from  '@angular/router';
 
 import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { ElementsComponent } from './components/elements/elements.component';
 
 @NgModule({
   imports:      [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },
+      { path: 'elements', component: ElementsComponent },
+      { path: '', component: WelcomeComponent }
+    ])
   ],
-  declarations: [ AppComponent ],
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    ElementsComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ app/components/elements/elements.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-elements',
+  templateUrl: 'elements.component.html'
+})
+
+export class ElementsComponent {
+
+}
