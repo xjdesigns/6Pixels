477f1b6b67ec586af3b0840833af22f90f39da20
Jason Jacobson <jdesigns.jason@gmail.com>
2017-01-30 09:06:41 -0600

    added grid table

--- app/components/grid/grid.component.ts
+++ app/components/grid/grid.component.ts
@@ -5,4 +5,39 @@ import { Component } from '@angular/core';
   selector: 'app-grid',
   templateUrl: 'grid.component.html'
 })
-export class GridComponent {}
+export class GridComponent {
+  description = [{
+    add: 'flex',
+    desc: 'Base class'
+  }, {
+    add: 'flex__i--{size}',
+    desc: 'Flex item'
+  }, {
+    add: 'flex__o--{size}',
+    desc: 'Flex offset'
+  }, {
+    add: 'flex--{size}-start',
+    desc: 'Aligns using justify-content:flex-start'
+  }, {
+    add: 'flex--{size}-center',
+    desc: 'Aligns using justify-content:center'
+  }, {
+    add: 'flex--{size}-end',
+    desc: 'Aligns using justify-content:flex-end'
+  }, {
+    add: 'flex--{size}-around',
+    desc: 'Aligns using justify-content:space-around'
+  }, {
+    add: 'flex--{size}-between',
+    desc: 'Aligns using justify-content:space-between'
+  }, {
+    add: 'flex--{size}-top',
+    desc: 'Aligns using align-items:flex-start'
+  }, {
+    add: 'flex--{size}-middle',
+    desc: 'Aligns using align-items:center'
+  }, {
+    add: 'flex--{size}-bottom',
+    desc: 'Aligns using align-items:flex-end'
+  }];
+}
