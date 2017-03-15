17bb5712c7a795b81ced15e5cdecc28add51c2d1
Jason Jacobson <jdesigns.jason@gmail.com>
2017-03-15 16:03:32 -0500

    cleanup

--- app/NG2/selectListDisplay.component.js
+++ app/NG2/selectListDisplay.component.js
@@ -5,7 +5,10 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var core_1 = require("@angular/core");
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
 var SelectlistDisplayComponent = (function () {
     function SelectlistDisplayComponent() {
         this.selectedListString = '123123433';
@@ -64,13 +67,14 @@ var SelectlistDisplayComponent = (function () {
     SelectlistDisplayComponent.prototype.selectList = function (val) {
         console.log('This is the event from the select list.', val);
     };
+    SelectlistDisplayComponent = __decorate([
+        core_1.Component({
+            moduleId: module.id,
+            selector: 'app-selectlist-display',
+            templateUrl: 'selectListDisplay.component.html'
+        }), 
+        __metadata('design:paramtypes', [])
+    ], SelectlistDisplayComponent);
     return SelectlistDisplayComponent;
 }());
-SelectlistDisplayComponent = __decorate([
-    core_1.Component({
-        moduleId: module.id,
-        selector: 'app-selectlist-display',
-        templateUrl: 'selectListDisplay.component.html'
-    })
-], SelectlistDisplayComponent);
 exports.SelectlistDisplayComponent = SelectlistDisplayComponent;
