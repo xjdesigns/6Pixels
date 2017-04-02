9b4bb0ca8864d864584626ef53fd11a1b014257d
Jason Jacobson <jdesigns.jason@gmail.com>
2017-04-02 08:04:09 -0500

    small tweak

--- app/NG2/carousel-display.component.js
+++ app/NG2/carousel-display.component.js
@@ -5,8 +5,10 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-var core_1 = require("@angular/core");
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
 var CarouselDisplayComponent = (function () {
     function CarouselDisplayComponent() {
         // images for display with carousel
@@ -35,13 +37,14 @@ var CarouselDisplayComponent = (function () {
                 desc: 'Optional value for your Alternate Images.'
             }];
     }
+    CarouselDisplayComponent = __decorate([
+        core_1.Component({
+            moduleId: module.id,
+            selector: 'app-carousel-display',
+            templateUrl: 'carousel-display.component.html'
+        }), 
+        __metadata('design:paramtypes', [])
+    ], CarouselDisplayComponent);
     return CarouselDisplayComponent;
 }());
-CarouselDisplayComponent = __decorate([
-    core_1.Component({
-        moduleId: module.id,
-        selector: 'app-carousel-display',
-        templateUrl: 'carousel-display.component.html'
-    })
-], CarouselDisplayComponent);
 exports.CarouselDisplayComponent = CarouselDisplayComponent;
